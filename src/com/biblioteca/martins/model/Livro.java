b31cbb1e5d87f88afd1b783a0edf38c8b6a7e84f
MartinsBackend <r.martins.programmer@gmail.com>
2025-09-15 17:28:24 -0300

    feat(model): adiciona a classe Livro seus atributos, construtor, getters e setters

---
+++ src/com/biblioteca/martins/model/Livro.java
@@ -0,0 +1,56 @@
+package com.biblioteca.martins.model;
+
+public class Livro {
+    private static int proximoId = 0;
+    private final int id;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        proximoId++;
+        this.id = proximoId;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+}
