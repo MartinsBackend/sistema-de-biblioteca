064369386621a136822e94ceacebe7d53af50fd7
MartinsBackend <r.martins.programmer@gmail.com>
2025-09-18 18:07:32 -0300

    feat(service): adiciona a classe Biblioteca e seus atributos

---
+++ src/com/biblioteca/martins/service/Biblioteca.java
@@ -0,0 +1,11 @@
+package com.biblioteca.martins.service;
+
+import com.biblioteca.martins.model.Livro;
+import com.biblioteca.martins.model.Usuario;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList <Livro> livros = new ArrayList<>();
+    private ArrayList <Usuario> usuarios = new ArrayList<>();
+}
