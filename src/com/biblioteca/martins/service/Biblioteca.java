c6c612f1480e7ecb32d8c04295c57484efb79d51
MartinsBackend <r.martins.programmer@gmail.com>
2025-09-18 18:19:23 -0300

    feat(service): adiciona o método adicionarLivro na classe Biblioteca

--- src/com/biblioteca/martins/service/Biblioteca.java
+++ src/com/biblioteca/martins/service/Biblioteca.java
@@ -6,6 +6,15 @@ import com.biblioteca.martins.model.Usuario;
 import java.util.ArrayList;
 
 public class Biblioteca {
-    private ArrayList <Livro> livros = new ArrayList<>();
-    private ArrayList <Usuario> usuarios = new ArrayList<>();
+    private ArrayList<Livro> livros = new ArrayList<>();
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    public boolean adicionarLivro(Livro l) {
+        if (l == null) {
+            return false;
+        }
+        livros.add(l);
+        return true;
+    }
+
 }
